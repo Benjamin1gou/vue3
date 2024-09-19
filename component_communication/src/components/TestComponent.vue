ca037719597aa9abbf35d8223253129816d8badc
yankuma4@gmail.com <yankuma4@gmail.com>
2024-09-19 18:34:14 +0900

    add 子コンポーネントを追加

---
+++ component_communication/src/components/TestComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p style="color:red">子コンポーネントに記載:{{ text }}</p>
+    </div>
+</template>
+  
+<script>
+    export default {
+        props: ['text']
+    };
+</script>
+  
