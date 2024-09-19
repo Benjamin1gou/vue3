79fa4263efc45e64c1dc6fae890f6873dcf866cc
yankuma4@gmail.com <yankuma4@gmail.com>
2024-09-19 18:36:33 +0900

    change 各コメントを追加

--- component_communication/src/App.vue
+++ component_communication/src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
+    <!-- 親コンポーネントのメッセージを表示 -->
     <h1>親コンポーネントに記載：{{ message }}</h1>
+    <!-- 子コンポーネントにメッセージをプロパティとして渡す -->
     <MyComponent :text="message" />
   </div>
 </template>
 
 <script>
+// 子コンポーネントMyComponentをインポート
 import MyComponent from './components/TestComponent.vue';
 
 export default {
+  // 子コンポーネントを登録
   components: {
     MyComponent
   },
+  // 親コンポーネントのデータを定義
   data() {
     return {
+      // 親コンポーネントから渡すメッセージ
       message: '親コンポーネントからのメッセージ'
     };
   }
--- component_communication/src/components/TestComponent.vue
+++ component_communication/src/components/TestComponent.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
+        <!-- 子コンポーネントのテキストを赤色で表示 -->
         <p style="color:red">子コンポーネントに記載:{{ text }}</p>
     </div>
 </template>
   
 <script>
     export default {
+        // 親コンポーネントから渡されるプロパティ "text" を受け取る
         props: ['text']
     };
 </script>
-  
