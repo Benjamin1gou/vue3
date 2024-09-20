1482fdc8a61c1ddf77a8a4f34b92aebd8f8d6817
yankuma4@gmail.com <yankuma4@gmail.com>
2024-09-20 16:25:54 +0900

    change router処理追加

--- router_test/src/App.vue
+++ router_test/src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-link to="/">ホーム</router-link> | 
+    <router-link to="/about">アバウト</router-link>
+    <router-view></router-view>  <!-- ここでルートコンポーネントが表示されます -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'App'
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- router_test/src/main.js
+++ router_test/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// VueのcreateApp関数をインポート
+import { createApp } from 'vue';
+// ルートコンポーネントAppをインポート
+import App from './App.vue';
+// ルーター設定をインポート
+import router from './router';
 
-createApp(App).mount('#app')
+// Vueアプリケーションのインスタンスを作成
+createApp(App)
+  // ルーターをアプリケーションに登録
+  .use(router)
+  // アプリケーションをHTMLの #app にマウント
+  .mount('#app');
