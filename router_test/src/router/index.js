3f31e08b01fb9065c27fed060305f16deebce649
yankuma4@gmail.com <yankuma4@gmail.com>
2024-09-24 12:18:01 +0900

    change 認証確認処理の追加

--- router_test/src/router/index.js
+++ router_test/src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// HomeおよびAboutコンポーネントをインポート
+// HomePageとAboutPageコンポーネントをインポート
 import Home from '../views/HomePage.vue';
 import About from '../views/AboutPage.vue';
 
 // ルート定義
 const routes = [
-  // ルートパス '/' に対応するコンポーネントは Home
-  { path: '/', component: Home },
-  // ルートパス '/about' に対応するコンポーネントは About
-  { path: '/about', component: About }
+  // ホームページのルート設定
+  { path: '/', name: 'Home', component: Home },
+  // Aboutページのルート設定。requiresAuthメタフィールドを追加して認証が必要であることを示す
+  { path: '/about', name: 'About', component: About, meta: { requiresAuth: true } }
 ];
 
 // Vue Routerのインスタンスを作成
@@ -19,5 +19,22 @@ const router = createRouter({
   routes
 });
 
-// ルーターをエクスポート
+// グローバルナビゲーションガード
+router.beforeEach((to, from, next) => {
+  // requiresAuthが設定されているルートの場合に認証チェックを行う
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    const isAuthenticated = false;  // ここに認証のロジックを追加
+    if (!isAuthenticated) {
+      // 認証されていない場合はホームページにリダイレクト
+      next('/');
+    } else {
+      // 認証されている場合はそのまま進む
+      next();
+    }
+  } else {
+    // 認証が不要なページへはそのまま進む
+    next();
+  }
+});
+
 export default router;
