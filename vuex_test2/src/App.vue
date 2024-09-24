e41088da65756ae5b2012dd305d5c2267f352e1c
yankuma4@gmail.com <yankuma4@gmail.com>
2024-09-24 18:17:06 +0900

    change フォームのハンドリングの追加

--- vuex_test2/src/App.vue
+++ vuex_test2/src/App.vue
@@ -13,6 +13,17 @@
     <!-- ボタンをクリックして非同期でカウントをインクリメント -->
     <button @click="incrementAsync">非同期でインクリメント</button>
   </div>
+  <div>
+    <!-- 固定メッセージを表示 -->
+    <p>{{ message }}</p>
+  </div>
+  <div>
+    <form @submit.prevent="submitForm">
+      <input v-model="name" placeholder="名前を入力してください" />
+      <button type="submit">送信</button>
+    </form>
+    <p>入力された名前: {{ name }}</p>
+  </div>
 </template>
 
 <script>
@@ -30,7 +41,33 @@ export default {
   // Vuexのアクションをメソッドとしてマッピング
   methods: {
     // アクション incrementAsync をマッピングして利用
-    ...mapActions(['incrementAsync'])
+    ...mapActions(['incrementAsync']),
+    submitForm() {
+      console.log('フォームが送信されました: ', this.name);
+    }
+  },
+  // ローカルデータとしてメッセージを定義
+  data() {
+    return {
+      message: 'こんにちは！', // 固定メッセージ
+      name: ''
+    };
+  },
+  // コンポーネントが作成されたときに実行されるライフサイクルフック
+  created() {
+    console.log('コンポーネントが作成されました');
+  },
+  // コンポーネントがマウントされたときに実行されるライフサイクルフック
+  mounted() {
+    console.log('コンポーネントがマウントされました');
+  },
+  // データが更新されたときに実行されるライフサイクルフック
+  updated() {
+    console.log('データが更新されました');
+  },
+  // コンポーネントが破棄される前に実行されるライフサイクルフック
+  beforeUnmount() {
+    console.log('コンポーネントが破棄されます');
   }
 };
 </script>
